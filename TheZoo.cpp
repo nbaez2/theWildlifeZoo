9a426a516c50eb130581f11c55cea260811c0dbe
Nicholas Baez <51276366+nbaez2@users.noreply.github.com>
2020-04-24 11:50:22 -0500

    roughDraft

---
+++ TheZoo.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <jni.h>
+using namespace std;
+
+void GenerateData()               //DO NOT TOUCH CODE IN THIS METHOD
+{
+     JavaVM *jvm;                      // Pointer to the JVM (Java Virtual Machine)
+     JNIEnv *env;                      // Pointer to native interface
+                                                              //================== prepare loading of Java VM ============================
+     JavaVMInitArgs vm_args;                        // Initialization arguments
+     JavaVMOption* options = new JavaVMOption[1];   // JVM invocation options
+     options[0].optionString = (char*) "-Djava.class.path=";   // where to find java .class
+     vm_args.version = JNI_VERSION_1_6;             // minimum Java version
+     vm_args.nOptions = 1;                          // number of options
+     vm_args.options = options;
+     vm_args.ignoreUnrecognized = false;     // invalid options make the JVM init fail
+                                                                          //=============== load and initialize Java VM and JNI interface =============
+     jint rc = JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args);  // YES !!
+     delete options;    // we then no longer need the initialisation options.
+     if (rc != JNI_OK) {
+            // TO DO: error processing...
+            cin.get();
+            exit(EXIT_FAILURE);
+     }
+     //=============== Display JVM version =======================================
+     cout << "JVM load succeeded: Version ";
+     jint ver = env->GetVersion();
+     cout << ((ver >> 16) & 0x0f) << "." << (ver & 0x0f) << endl;
+
+     jclass cls2 = env->FindClass("ZooFileWriter");  // try to find the class
+     if (cls2 == nullptr) {
+            cerr << "ERROR: class not found !";
+     }
+     else {                                  // if class found, continue
+            cout << "Class MyTest found" << endl;
+            jmethodID mid = env->GetStaticMethodID(cls2, "createZooFile", "()V");  // find method
+            if (mid == nullptr)
+                   cerr << "ERROR: method void createZooFile() not found !" << endl;
+            else {
+                   env->CallStaticVoidMethod(cls2, mid);                      // call method
+                   cout << endl;
+            }
+     }
+
+
+     jvm->DestroyJavaVM();
+     cin.get();
+}
+
+void AddAnimal()
+{
+     /*
+            TODO: Write proper code to add an animal to your vector (or array)
+     */
+}
+
+
+void RemoveAnimal()
+{
+     /*
+            TODO: Write proper code to remove an animal from your vector (or array. Remmber to re-allocate proper size if using array)
+     */
+}
+
+void LoadDataFromFile()
+{
+     /*
+            TODO: Write proper code to load data from input file (generated using JNI) into vector/array.
+     */
+}
+
+void SaveDataToFile()
+{
+     /*
+            TODO: Write proper code to store vector/array to file.
+     */
+}
+
+void DisplayMenu()
+{
+     /*
+            TODO: write proper code to display menu to user to select from
+     */
+}
+
+
+
+int main()
+{
+	GenerateData();
+	return 1;
+}
